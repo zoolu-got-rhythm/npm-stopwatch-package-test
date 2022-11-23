0fd7d8f4731edbef75fd9958ff23de2942e03ee9
christo morris <christo@christos-MacBook-Pro.local>
2022-11-23 02:58:26 +0000

    updated src/index.ts package usage code

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,12 @@ async function sleep(delayInMs: number): Promise<number>{
 (async () => {
     console.log("https://www.npmjs.com/package/@slime/stopwatch demo");
     const stopWatch1 = new StopWatch();
+    console.log("start timer");
     stopWatch1.startTimer();
     await sleep(1500); 
     stopWatch1.stopTimer();
-    console.log(stopWatch1.getTimeElapsedInMs);
+    console.log("stop timer");
+    console.log("time elapsed:");
+    console.log(stopWatch1.getTimeElapsedInMs + " milliseconds");
     console.log(stopWatch1.getTimeElapsedInStopWatchFormatString);
 })();
